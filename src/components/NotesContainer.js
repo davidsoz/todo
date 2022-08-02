4454a41dff7c6ea06b7dd6c6887f94af20375c6d
Vakho Nakashidze <vakhonakashidze@Vakhos-MacBook-Pro.local>
2022-08-03 01:33:01 +0400

    made by Vakho

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -4,157 +4,177 @@ import { useEffect, useRef, useState } from "react";
 import ActiveNote from "./ActiveNote";
 
 const Container = styled.div`
-  width: 750px;
-  margin: 30px auto;
-  padding: 10px 5px;
-  background-color: #fff;
-  box-shadow: 0px 0px 15px 2px rgba(71, 70, 70, 0.75);
-  > .active-notes {
-    max-height: 350px;
-    overflow-y: scroll;
-  }
+	width: 750px;
+	margin: 30px auto;
+	padding: 10px 5px;
+	background-color: #fff;
+	box-shadow: 0px 0px 15px 2px rgba(71, 70, 70, 0.75);
+	> .active-notes {
+		max-height: 350px;
+		overflow-y: scroll;
+	}
 `;
 
 const TodoFooter = styled.div`
-  display: flex;
-  padding: 10px;
-  justify-content: space-between;
-  .actions {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    > div:first-child {
-      text-align: center;
-      font-weight: bold;
-    }
-    > div.colors {
-      display: flex;
-      flex-direction: column;
-      gap: -10px;
-    }
-  }
+	display: flex;
+	padding: 10px;
+	justify-content: space-between;
+	.actions {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		> div:first-child {
+			text-align: center;
+			font-weight: bold;
+		}
+		> div.colors {
+			display: flex;
+			flex-direction: column;
+			gap: -10px;
+		}
+	}
 `;
 
 const InputProps = {
-  style: { fontSize: 36, color: "#8f8f89", paddingLeft: 50 },
+	style: { fontSize: 36, color: "#8f8f89", paddingLeft: 50 },
 };
 
 function NotesContainer() {
-  const [notes, setNotes] = useState([]);
-  // const [checked, setChecked] = useState(false);
-  const [noteText, setNoteText] = useState(``);
+	const [notes, setNotes] = useState([]);
+	const [noteText, setNoteText] = useState(``);
+  const [filterColors, setFilterColors] = useState([]);
+  const [filteredNotes, setFilteredNotes] = useState([]);
 
-  const menuItems = [
-    { color: "green", value: 1, label: "Green" },
-    { color: "blue", value: 2, label: "Blue" },
-    { color: "orange", value: 3, label: "Orange" },
-    { color: "purple", value: 4, label: "Purple" },
-    { color: "red", value: 5, label: "Red" },
-  ];
+	const menuItems = [
+		{ value: "green", label: "Green" },
+		{ value: "blue", label: "Blue" },
+		{ value: "orange", label: "Orange" },
+		{ value: "purple", label: "Purple" },
+		{ value: "red", label: "Red" },
+	];
 
-  function moveToActive(e) {
-    if (e.key === "Enter" && noteText.length > 2) {
-      e.preventDefault();
-      setNotes([
-        ...notes,
-        {
-          text: noteText,
-          id: !notes.length ? 0 : notes[notes.length - 1].id + 1,
-          // checked: checked,
-        },
-      ]);
-      setNoteText(``);
+	function moveToActive(e) {
+		if (e.key === "Enter" && noteText.length > 2) {
+			e.preventDefault();
+			setNotes([
+				...notes,
+				{
+					text: noteText,
+					id: notes[notes.length - 1]?.id + 1 || 0,
+          color: '',
+					completed: false,
+				},
+			]);
+			setNoteText(``);
+		}
+	}
+
+	function removeActiveNote(id) {
+		const filteredNotes = notes.filter(
+			(note) => note.id !== Number(id)
+		);
+		setNotes(filteredNotes);
+	}
+
+  function updateNoteProp(id, prop, value) { // 
+    const nextNotes = [...notes];
+    const noteIndex = notes.findIndex(note => note.id === id);
+    const nextNote = {
+      ...notes[noteIndex],
+      [prop]: value
+    };
+
+    nextNotes.splice(noteIndex, 1, nextNote);
+
+    setNotes(nextNotes);
+  }
+
+  function updateColor(id, color) {
+    updateNoteProp(id, 'color', color);
+  }
+
+  function toggleFilterColor(color) {
+    let nextFilterColors = [...filterColors];
+    if(nextFilterColors.includes(color)) {
+      nextFilterColors = nextFilterColors.filter(c => c !== color);
+    } else {
+      nextFilterColors.push(color);
     }
+
+    setFilterColors(nextFilterColors);
   }
 
-  function removeActiveNote(e) {
-    const filteredNotes = notes.filter(
-      (note) => note.id !== Number(e.target.id)
-    );
-    setNotes(filteredNotes);
+  function filterByColors() {
+    
   }
 
- 
 
-  return (
-    <Container>
-      <div>
-        <TextField
-          autoFocus
-          variant="standard"
-          fullWidth
-          placeholder="What needs to be done?"
-          onChange={(e) => setNoteText(e.target.value)}
-          value={noteText}
-          InputProps={InputProps}
-          onKeyPress={(e) => moveToActive(e)}
-        />
-      </div>
-      <div className="active-notes">
-        {notes.map((note) => (
-          <ActiveNote
-            removeActiveNote={removeActiveNote}
-            key={note.id}
-            id={note.id}
-            label={note.text}
-            // checked={checked}
-            // setChecked={setChecked}
-            menuItems={menuItems}
-          />
-        ))}
-      </div>
-      {notes.length ? (
-        <TodoFooter>
-          <div className="actions">
-            <div>Actions</div>
-            <Button variant="contained" size="small">
-              mark all completed
-            </Button>
-            <Button variant="contained" size="small">
-              clear completed
-            </Button>
-          </div>
-          <div className="actions">
-            <div>Remaining todos</div>
-            <div>{notes.length} Item Left</div>
-          </div>
-          <div className="actions">
-            <div>Filter By Status</div>
-            <div>All</div>
-            <div>Active</div>
-            <div>Completed</div>
-          </div>
-          <div className="actions">
-            <div>Filter By Color</div>
-            <div className="colors">
-              {menuItems.map((item) => (
-                <FormControlLabel
-                  key={item.value}
-                  control={<Checkbox onChange={() => {}} />}
-                  label={item.label}
-                />
-              ))}
-            </div>
-          </div>
-        </TodoFooter>
-      ) : null}
-    </Container>
-  );
+
+	return (
+		<Container>
+			<div>
+				<TextField
+					autoFocus
+					variant="standard"
+					fullWidth
+					placeholder="What needs to be done?"
+					onChange={(e) => setNoteText(e.target.value)}
+					value={noteText}
+					InputProps={InputProps}
+					onKeyPress={(e) => moveToActive(e)}
+				/>
+			</div>
+			<div className="active-notes">
+				{notes.map((note) => (
+					<ActiveNote
+						removeActiveNote={() => removeActiveNote(note.id)}
+						key={note.id}
+						note={note}
+            onSelect={() => updateNoteProp(note.id, 'completed', !note.completed)}
+            onColorSelect={updateColor}
+						// checked={checked}
+						// setChecked={setChecked}
+						menuItems={menuItems}
+					/>
+				))}
+			</div>
+			{notes.length ? (
+				<TodoFooter>
+					<div className="actions">
+						<div>Actions</div>
+						<Button variant="contained" size="small">
+							mark all completed
+						</Button>
+						<Button variant="contained" size="small">
+							clear completed
+						</Button>
+					</div>
+					<div className="actions">
+						<div>Remaining todos</div>
+						<div>{notes.length} Item Left</div>
+					</div>
+					<div className="actions">
+						<div>Filter By Status</div>
+						<div>All</div>
+						<div>Active</div>
+						<div>Completed</div>
+					</div>
+					<div className="actions">
+						<div>Filter By Color</div>
+						<div className="colors">
+							{menuItems.map((item) => (
+								<FormControlLabel
+									key={item.value}
+									control={<Checkbox onChange={() => toggleFilterColor(item.value)} />}
+									label={item.label}
+								/>
+							))}
+						</div>
+					</div>
+				</TodoFooter>
+			) : null}
+		</Container>
+	);
 }
 
 export default NotesContainer;
-
-
-function solution(A) {
-  let i=1;
-  let minNum
-  while(i<100000) {
-      for(let j=0; j<A.length; j++){
-          if(i != A[j] && Math.sign(A[j]) == 1){
-              minNum=i;
-          }
-      }
-      i++
-  }
-  return minNum
-}
--- src/components/activeNote.js
+++ src/components/activeNote.js
@@ -1,77 +1,74 @@
 import {
-  Button,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  InputLabel,
-  MenuItem,
-  Select,
+	Button,
+	Checkbox,
+	FormControl,
+	FormControlLabel,
+	InputLabel,
+	MenuItem,
+	Select,
 } from "@mui/material";
 
 import styled from "styled-components";
 
 const Active = styled.div`
-  display: flex;
-  padding: 0 10px;
-  justify-content: space-between;
-  border-bottom: 2px solid lightgray;
-  > div:last-child {
-    display: flex;
-    align-items: center;
-  }
+	display: flex;
+	padding: 0 10px;
+	justify-content: space-between;
+	border-bottom: 2px solid lightgray;
+	> div:last-child {
+		display: flex;
+		align-items: center;
+	}
 `;
 
 function ActiveNote({
-  id,
-  label,
-  setChecked,
-  checked,
-  menuItems,
-  removeActiveNote,
+	note,
+	onSelect,
+	menuItems,
+	removeActiveNote,
+	onColorSelect
 }) {
-
-  return (
-    <Active>
-      <FormControlLabel
-        control={<Checkbox key={id} />}
-        label={label}
-      />
-      <div>
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-          <InputLabel id="demo-select-small">color</InputLabel>
-          <Select
-            labelId="demo-select-small"
-            id="demo-select-small"
-            label="color"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            {menuItems.map((menuItem) => {
-              return (
-                <MenuItem
-                  key={menuItem.value}
-                  style={{ color: menuItem.color }}
-                  value={menuItem.value}
-                >
-                  {menuItem.label}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <div>
-          <Button
-            id={id}
-            onClick={(e) => removeActiveNote(e)}
-            style={{ color: "#df03fc" }}
-          >
-            &#10005;
-          </Button>
-        </div>
-      </div>
-    </Active>
-  );
+	const {id, text: label, color, completed} = note;
+	return (
+		<Active>
+			<FormControlLabel control={<Checkbox key={note.id} onChange={onSelect} checked={completed} />} label={label} />
+			<div>
+				<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+					<InputLabel id="demo-select-small">color</InputLabel>
+					<Select
+						labelId="demo-select-small"
+						id="demo-select-small"
+						label="color"
+						value={color}
+						onChange={e => onColorSelect(id, e.target.value)}
+					>
+						<MenuItem value="">
+							<em>None</em>
+						</MenuItem>
+						{menuItems.map((menuItem) => {
+							return (
+								<MenuItem
+									key={menuItem.value}
+									style={{ color: menuItem.value }}
+									value={menuItem.value}
+								>
+									{menuItem.label}
+								</MenuItem>
+							);
+						})}
+					</Select>
+				</FormControl>
+				<div>
+					<Button
+						onClick={() => removeActiveNote(id)}
+						style={{ color: "#df03fc" }}
+					>
+						&#10005;
+					</Button>
+				</div>
+			</div>
+		</Active>
+	);
 }
 
 export default ActiveNote;
